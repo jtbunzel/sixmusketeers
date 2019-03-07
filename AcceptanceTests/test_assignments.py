310f3fe2bdf4397af1e9d53e690895722d122e88
Jonathan Gomez <Gomez7@uwm.edu>
2019-03-06 22:31:50 -0600

    Created acceptance test file
    Signed-off-by: Jonathan Gomez <Gomez7@uwm.edu>

---
+++ AcceptanceTests/test_assignments.py
@@ -0,0 +1,11 @@
+# 17A TA will be able to view his course assignments (and not change it) so that he can plan his semester.
+import unittest
+
+
+class TestAssignments(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
