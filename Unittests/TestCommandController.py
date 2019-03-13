222ecea3b1bea894596d12c571b99a7469a45d5a
Ritirose <bijayakhanal652@yahoo.com>
2019-03-12 21:35:25 -0500

    bijay's push

--- Unittests/TestCommandController.py
+++ Unittests/TestCommandController.py
@@ -3,6 +3,11 @@ from Skeleton_Classes.CommandController import CommandController
 from User import *
 from Administrator import *
 from Course import *
+from Instructor import *
+from Lab_section import *
+from Supervisor import *
+from TA import *
+
 class TestCommandController(bk.TestCase):
     def setup(self):
         self.command= CommandController()
