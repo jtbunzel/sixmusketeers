a63b840c2db275bcb89fed445d8c81eae1133d11
Jonathan <Gomez7@uwm.edu>
2019-03-26 12:28:56 -0500

    klarjk;ehkrhj

---
+++ database/untitled/TestTA.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+import Skeleton_Classes.TA
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        TA.objects.create(name="lion", sound="roar")
+        TA.objects.create(name="cat", sound="meow")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = TA.objects.get(name="lion")
+        cat = TA.objects.get(name="cat")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
+
+if __name__ == '__main__':
+    unittest.main()
