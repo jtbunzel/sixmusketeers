fe418afba90aa08d83429c53031335892e7b45eb
HRBlockhead <dtsp1198@gmail.com>
2019-03-28 11:18:12 -0500

    Merge branch 'master' of C:\Users\dtsp\PycharmProjects\sixmusketeers with conflicts.

--- database/school_database/models.py
+++ database/school_database/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user(models.model)
+    username = models.CharField(max_length = 30)
+    password = models.CharField(max_length = 30)
+    full_name = models.CharFiled(max_length = 40)
+    address = models
+
+
