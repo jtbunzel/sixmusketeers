1fde48c1410e49da14edada2e08b6198ce44532f
bijaya12345 <47364203+bijaya12345@users.noreply.github.com>
2019-04-02 21:47:17 -0500

    Update Administrator.py

--- Skeleton_Classes/Administrator.py
+++ Skeleton_Classes/Administrator.py
@@ -4,40 +4,41 @@ from Skeleton_Classes.User import *
 class Administrator(User):
 
     def __init__(self, username, password):
-        pass
+        self.username=username
+        self.password=password
 
     def __str__(self):
         pass
 
     def get_instructor(self):
-        return " "
+        return self.instructor
 
     def create_instructor(self, new_instructor ):
-        pass
+        self.instructor=new_instructor
 
     def remove_instructor(self, instructor_to_be_removed):
-        pass
+        self.instructor=instructor_to_be_removed
 
     def edit_instructor(self, instructor_to_be_edit):
-        pass
+        self.instructor=instructor_to_be_edit
 
     def assign_instructor_to_course(self, instructor_to_be_assign, course):
         pass
 
     def get_courses(self):
-        return " "
+        return self.course
 
     def create_course(self, new_course):
-        pass
+        self.course=new_course
 
     def remove_course(self, course_to_be_removed):
         pass
 
     def get_public_contact_info(self):
-        return " "
+        return self.info
 
     def get_lab_sections(self):
-        return " "
+        return ""
 
     def create_lab_section(self, course, section):
         pass
