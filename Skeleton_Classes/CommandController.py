b0498fdd57545bc21d0dc2665b92db74d7536383
HRBlockhead <dtsp1198@gmail.com>
2019-04-04 14:49:18 -0500

    I think I wrote logout as well. Hopefully we can see some functionality soon...?

--- Skeleton_Classes/CommandController.py
+++ Skeleton_Classes/CommandController.py
@@ -1,11 +1,27 @@
+from Skeleton_Classes.Database import *
+from Skeleton_Classes.App import *
+
+
 class CommandController(object):
+
+    def __init__(self, app):
+        self.database_interface = Database()
+        self.app = app
 
     def parse(self, a):
         self.command= a
 
     def login(self, username, password):
-        self.login.username = username
-        self.login.password = password
+        if self.app.get_loggedin() is not None:
+            return 'User already logged in. Log out to log in as a different user.'
+        user_logging_in = self.database_interface.read('username=username')
+        if user_logging_in is None:
+            return 'User not found.'
+        if user_logging_in.password != password:
+            return 'Password is incorrect.'
+        self.app.set_loggedin(user_logging_in)
+        return
+    pass
 
     def create(self, credentials):
         pass
@@ -17,6 +33,10 @@ class CommandController(object):
         pass
 
     def logout(self, username):
+        user_to_be_saved = self.app.get_loggedin()
+        self.database_interface.write(user_to_be_saved)
+        self.app.set_loggedin(None)
+        return 'User logged out.'
         pass
 
     def edit(self, accountDetails, newDetails):
