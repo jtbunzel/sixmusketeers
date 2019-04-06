477f16fcf1a43814c9964862847b8cf6dc28786d
BinodChhantyal <bishnu@uwm.edu>
2019-04-06 18:08:12 -0500

    verify

--- Skeleton_Classes/CommandController.py
+++ Skeleton_Classes/CommandController.py
@@ -91,3 +91,218 @@ class CommandController(object):
 
     def assignments(dataType):
         pass
+    def verify(self, user, a):
+        if (user.rank <3):
+
+            if (self.parse(a)== 'create_TA'):
+                return True
+            if (self.parse(a)== 'create_instructor'):
+                return True
+            if (self.parse(a)== 'create_course'):
+                return True
+            if (self.parse(a)== 'create_labSection'):
+                return True
+            if (self.parse(a) == 'create_supervisor'):
+                return False
+
+            if (self.parse(a)== 'notify_admin'):
+                return True
+            if (self.parse(a)== 'notify_TA'):
+                return True
+            if (self.parse(a)== 'notify_instructor'):
+                return True
+
+            if (self.parse(a)== 'assign_TA_labSection'):
+                return True
+            if (self.parse(a)== 'assign_instructor_cousre'):
+                return True
+            if (self.parse(a) == 'assign_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'assign_supervisor'):
+                return False
+
+            if (self.parse(a)== 'edit_TA'):
+                return True
+            if (self.parse(a)== 'edit_instructor'):
+                return True
+            if (self.parse(a)== 'edit_course'):
+                return True
+            if (self.parse(a) == 'edit_labSection'):
+                return True
+
+            if (self.parse(a) == 'remove_TA'):
+                return True
+            if (self.parse(a) == 'remove_instructor'):
+                return True
+            if (self.parse(a) == 'remove_course'):
+                return True
+            if (self.parse(a) == 'remove_labSection'):
+                return True
+            if (self.parse(a) == 'remove_supervisor'):
+                return False
+
+            if (self.parse(a) == 'access_Ta'):
+                return True
+            if (self.parse(a) == 'access_instructor'):
+                return True
+            if (self.parse(a) == 'access_course'):
+                return True
+            if (self.parse(a) == 'access_labSection'):
+                return True
+
+        if (user.rank < 2):
+            if (self.parse(a) == 'create_administrator'):
+                return True
+            if (self.parse(a) == 'notify_administrator'):
+                return True
+            if (self.parse(a) == 'access_administrator'):
+                return True
+            if (self.parse(a) == 'edit_administrator'):
+                return True
+
+        if (user.rank == 3):
+            if (self.parse(a)== 'create_TA'):
+                return False
+            if (self.parse(a)== 'create_instructor'):
+                return False
+            if (self.parse(a)== 'create_course'):
+                return False
+            if (self.parse(a)== 'create_labSection'):
+                return False
+            if (self.parse(a) == 'create_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'create_supervisor'):
+                return False
+
+
+            if (self.parse(a)== 'notify_TA'):
+                return True
+            if (self.parse(a)== 'notify_instructor'):
+                return False
+            if (self.parse(a) == 'notify_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'notify_supervisor'):
+                return False
+
+
+            if (self.parse(a)== 'assign_TA_labSection'):
+                return True
+            if (self.parse(a)== 'assign_instructor_cousre'):
+                return True
+            if (self.parse(a) == 'assign_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'assign_supervisor'):
+                return False
+
+            if (self.parse(a)== 'edit_TA'):
+                return True
+            if (self.parse(a)== 'edit_instructor'):
+                return False
+            if (self.parse(a)== 'edit_course'):
+                return True
+            if (self.parse(a) == 'edit_labSection'):
+                return True
+            if (self.parse(a) == 'edit_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'edit_supervisor'):
+                return False
+
+            if (self.parse(a) == 'remove_TA'):
+                return False
+            if (self.parse(a) == 'remove_instructor'):
+                return False
+            if (self.parse(a) == 'remove_course'):
+                return False
+            if (self.parse(a) == 'remove_labSection'):
+                return False
+            if (self.parse(a) == 'remove_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'remove_supervisor'):
+                return False
+
+            if (self.parse(a) == 'access_Ta'):
+                return True
+            if (self.parse(a) == 'access_instructor'):
+                return False
+            if (self.parse(a) == 'access_course'):
+                return True
+            if (self.parse(a) == 'access_labSection'):
+                return True
+            if (self.parse(a) == 'access_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'access_supervisor'):
+                return False
+
+        if (user.rank == 4):
+            if (self.parse(a)== 'create_TA'):
+                return False
+            if (self.parse(a)== 'create_instructor'):
+                return False
+            if (self.parse(a)== 'create_course'):
+                return False
+            if (self.parse(a)== 'create_labSection'):
+                return False
+            if (self.parse(a) == 'create_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'create_supervisor'):
+                return False
+
+
+            if (self.parse(a)== 'notify_TA'):
+                return False
+            if (self.parse(a)== 'notify_instructor'):
+                return False
+            if (self.parse(a) == 'notify_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'notify_supervisor'):
+                return False
+
+
+            if (self.parse(a)== 'assign_TA_labSection'):
+                return False
+            if (self.parse(a)== 'assign_instructor_cousre'):
+                return False
+            if (self.parse(a) == 'assign_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'assign_supervisor'):
+                return False
+
+            if (self.parse(a)== 'edit_TA'):
+                return False
+            if (self.parse(a)== 'edit_instructor'):
+                return False
+            if (self.parse(a)== 'edit_course'):
+                return False
+            if (self.parse(a) == 'edit_labSection'):
+                return False
+            if (self.parse(a) == 'edit_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'edit_supervisor'):
+                return False
+
+            if (self.parse(a) == 'remove_TA'):
+                return False
+            if (self.parse(a) == 'remove_instructor'):
+                return False
+            if (self.parse(a) == 'remove_course'):
+                return False
+            if (self.parse(a) == 'remove_labSection'):
+                return False
+            if (self.parse(a) == 'remove_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'remove_supervisor'):
+                return False
+
+            if (self.parse(a) == 'access_Ta'):
+                return False
+            if (self.parse(a) == 'access_instructor'):
+                return False
+            if (self.parse(a) == 'access_course'):
+                return False
+            if (self.parse(a) == 'access_labSection'):
+                return False
+            if (self.parse(a) == 'access_Adminsistrator'):
+                return False
+            if (self.parse(a) == 'access_supervisor'):
+                return False
+
