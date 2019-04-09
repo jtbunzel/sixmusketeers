c177955988da54f8839b6ab9ac385c812db11457
HRBlockhead <dtsp1198@gmail.com>
2019-04-08 23:49:19 -0500

    Changing App

--- Skeleton_Classes/App.py
+++ Skeleton_Classes/App.py
@@ -5,10 +5,11 @@ from Skeleton_Classes.Course import *
 
 class App(object):
     def __init__(self):
+        self.command_controller = CommandController()
+        self.set_cmd_controller()
         pass
 
     def set_cmd_controller(self):
-        self.command_controller = CommandController()
         self.command_controller.set_pointer_to_app(self)
 
     def command(self, a):
