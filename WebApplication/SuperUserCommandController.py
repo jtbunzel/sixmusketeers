2bda3ef35a986b2664250b1dac87020fbe4d7f82
Jonathan Gomez <Gomez7@uwm.edu>
2019-04-13 00:56:33 -0500

    Added SuperUserCommandController Class Implemented deleteUser

---
+++ WebApplication/SuperUserCommandController.py
@@ -0,0 +1,30 @@
+from Skeleton_Classes.User import User
+from .models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class SuperUserCommandController:
+    user = User
+
+    def deleteUser(self, targetUser):
+        #Check for user logged in
+        if self.user is None:
+            return "You must be logged in"
+
+        #Check for Supervisor or Admin role
+        if self.user.rank < 2:
+            return "You do not have permission to use this command"
+
+        #Check if user exists
+        #if there is no Entry object with a primary key of 1, Django will raise Entry.DoesNotExist.
+        try:
+            currentUser = User.objects.get(username = targetUser)
+        except ObjectDoesNotExist:
+            print("User could not be found.")
+
+        if self.user.username == currentUser.username:
+            return "You can not delete your own account"
+
+        currentUser.delete()
+
+        return "User successfully deleted."
