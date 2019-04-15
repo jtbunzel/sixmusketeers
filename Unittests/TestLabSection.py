929052c104c693d23be68c5631651bee6a2ca4fb
Jonathan Gomez <Gomez7@uwm.edu>
2019-04-15 01:43:34 -0500

    TestCourse - Repaired TestCourse UnitTest TestLabSection - Repaired LabSection UnitTest

--- Unittests/TestLabSection.py
+++ Unittests/TestLabSection.py
@@ -4,18 +4,18 @@ from Application_Classes.LabSection import *
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.lab = LabSection("801", "Henry", "Intro to Software Engineering")
+        self.lab = LabSection("Henry", "801", "Intro to Software Engineering")
 
     def test_setUp(self):
-        self.assertEqual(self.lab.lab_number, "801")
         self.assertEqual(self.lab.TA, "Henry")
+        self.assertEqual(self.lab.lab_number, "801")
         self.assertEqual(self.lab.course, "Intro to Software Engineering")
-
-    def test_get_lab_number(self):
-        self.assertEqual(self.lab.get_lab_number(), "801")
 
     def test_get_lab_TA(self):
         self.assertEqual(self.lab.get_lab_TA(), "Henry")
+
+    def test_get_lab_number(self):
+        self.assertEqual(self.lab.get_lab_number(), "801")
 
     def test_get_lab_course(self):
         self.assertEqual(self.lab.get_lab_course(), "Intro to Software Engineering")
